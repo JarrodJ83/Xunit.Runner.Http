08723e56bb60ed20dfbb2a6d6b472f15ecdb9f15
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2023-12-10 15:03:04 -0500

    Create library

---
+++ Xunit.Runner.Http/HttpRunnerBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Xunit.Runners.Http;
+
+public static class HttpRunnerBuilderExtensions
+{
+    public static void AddXunitHttpTestRunner(this IServiceCollection services, Assembly testAssembly)
+    {
+        services.AddSingleton(provider => new HttpTestRunner(provider.GetRequiredService<ILogger<HttpTestRunner>>(), testAssembly));
+    }
+
+    public static void UseXunitHttpTestRunner(this IEndpointRouteBuilder app)
+    {
+        var httpTestTrigger = app.ServiceProvider.GetRequiredService<HttpTestRunner>();
+
+        var testCases = httpTestTrigger.Discover();
+
+        foreach (var testCase in testCases)
+        {
+            app.MapGet(testCase.DisplayName, (HttpRequest request) =>
+            {
+                var nameOfTestToRun = request.Path.Value.TrimStart('/');
+
+                return httpTestTrigger.RunTest(nameOfTestToRun);
+            }).WithName(testCase.DisplayName)
+            .WithOpenApi();
+        }
+    }
+}
---
+++ Xunit.Runner.Http/HttpTestRunner.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Xunit.Abstractions;
+
+namespace Xunit.Runners.Http
+{
+    internal class HttpTestRunner
+    {
+        ManualResetEvent finished = new ManualResetEvent(false);
+        ManualResetEvent discoveryComplete = new ManualResetEvent(false);
+        private readonly ILogger _logger;
+        private readonly CustomAssemblyRunner _runner;
+
+        public HttpTestRunner(ILogger logger, Assembly testAssembly)
+        {
+            _logger = logger;
+
+            var testAssemblyName = testAssembly.Location;
+
+            _runner = CustomAssemblyRunner.WithoutAppDomain(testAssemblyName);
+
+            _runner.OnDiscoveryComplete = OnDiscoveryComplete;
+            _runner.OnExecutionComplete = OnExecutionComplete;
+            _runner.OnTestFailed = OnTestFailed;
+            _runner.OnTestSkipped = OnTestSkipped;
+        }
+
+        public IReadOnlyCollection<ITestCase> Discover()
+        {
+            _runner.Discover();
+
+            discoveryComplete.WaitOne();
+
+            return _runner.TestCasesToRun;
+        }
+
+        public string RunTest(string testDisplayName)
+        {
+            var testToRun = _runner.TestCasesToRun.Single(tc => tc.DisplayName.Equals(testDisplayName));
+
+            var response = "Passed";
+
+            _runner.OnTestFailed = failed =>
+            {
+                response = "Failure: " + failed.ExceptionMessage + "\n" + failed.ExceptionStackTrace;
+            };
+
+            _runner.Run(new[] { testToRun });
+
+            finished.WaitOne();
+            finished.Reset();
+
+            return response;
+        }
+
+        void OnDiscoveryComplete(DiscoveryCompleteInfo info)
+        {
+            _logger.LogInformation("Running {TestCasesToRun} of {TestCasesDiscovered} tests...", info.TestCasesToRun, info.TestCasesDiscovered);
+            discoveryComplete.Set();
+        }
+
+        void OnExecutionComplete(ExecutionCompleteInfo info)
+        {
+            _logger.LogInformation("Finished: {TotalTests} tests in {ExecutionTime}s ({TestsFailed} failed, {TestsSkipped} skipped)",
+               info.TotalTests, Math.Round(info.ExecutionTime, 3), info.TestsFailed, info.TestsSkipped);
+
+            finished.Set();
+        }
+
+        void OnTestFailed(TestFailedInfo info)
+        {
+            _logger.LogError("{TestDisplayName} Failed: {ExceptionMessage}", info.TestDisplayName, $"[{info.ExceptionType}] {info.ExceptionMessage} :: {info.ExceptionStackTrace}");
+        }
+
+        void OnTestSkipped(TestSkippedInfo info)
+        {
+            _logger.LogWarning("[SKIP] {TestDisplayName}: {SkipReason}", info.TestDisplayName, info.SkipReason);
+        }
+    }
+}
