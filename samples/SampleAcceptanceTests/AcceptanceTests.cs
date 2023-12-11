d2902573bc4824a0dd4f14a792c8cc22fe199777
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2023-12-11 18:31:07 -0500

    Add sample project

---
+++ samples/SampleAcceptanceTests/AcceptanceTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace SampleAcceptanceTests;
+
+public class AcceptanceTests
+{
+    [Fact]
+    public void Test_One()
+    {
+
+    }
+
+    [Fact]
+    public void Test_Two()
+    {
+        Assert.Fail("Fail!");
+    }
+}
